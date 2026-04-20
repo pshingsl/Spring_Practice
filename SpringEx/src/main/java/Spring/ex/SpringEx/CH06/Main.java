723fa33c10d5589f7c656dc8db4d893ac63d4c3b
pshingsl <w455ok@gmail.com>
2026-04-20 17:08:14 +0900

    스프링 JDBC CRUD

--- SpringEx/src/main/java/Spring/ex/SpringEx/CH06/Main.java
+++ SpringEx/src/main/java/Spring/ex/SpringEx/CH06/Main.java
@@ -1,6 +1,10 @@
 package Spring.ex.SpringEx.CH06;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -10,8 +14,38 @@ public class Main {
         // 데이터베이스 연결 SQLException예외 발
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "1234");
 
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM member where id > ?");
-        preparedStatement.setLong(1, 2L);
+        // 멤베 테이블 생성 및 값 등록
+        Scanner sc = new Scanner(System.in);
+        List<Member> members = new ArrayList<>();
+
+        while (true) {
+            System.out.println("사용자를 등록 해주세요.");
+            String name = sc.next();
+            if(name.equals("exit")) break;
+
+            System.out.print("이메일: ");
+            String email = sc.next();
+
+            System.out.print("나이: ");
+            int age = sc.nextInt();
+
+            members.add(new Member(null, name, email, age));
+        }
+
+        PreparedStatement createStatement = connection.prepareStatement("insert into member(name, email, age) values(?,?,?)");
+        for (Member m : members) {
+            createStatement.setString(1, m.getName());
+            createStatement.setString(2, m.getEmail());
+            createStatement.setInt(3, m.getAge());
+            createStatement.addBatch();
+        }
+
+        int[] create = createStatement.executeBatch();
+        System.out.println("멤버 생성: " + Arrays.toString(create));
+
+        // 조회
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM member");
+//        preparedStatement.setLong(1, 2L);
         ResultSet result = preparedStatement.executeQuery();
 
         while (result.next()) {
@@ -23,7 +57,36 @@ public class Main {
             System.out.println("회원: " + member);
         }
 
+        // 업데이트
+        System.out.print("수정할 ID: ");
+        Long id = sc.nextLong();
 
+        System.out.println("수정할 이메일: ");
+        String email = sc.next();
+
+        System.out.println("수정할 나이: ");
+        Integer age = sc.nextInt();
+
+        System.out.println("수정할 이름: ");
+        String name = sc.next();
+
+        PreparedStatement updateStatement = connection.prepareStatement("UPDATE member SET name = ?, email = ?, age = ?  WHERE id = ?");
+        updateStatement.setString(1, name);
+        updateStatement.setString(2, email);
+        updateStatement.setInt(3, age);
+        updateStatement.setLong(4, id);
+
+        int updateResult = updateStatement.executeUpdate();
+        System.out.println("수정 후:" + updateResult);
+
+        // 삭제
+        System.out.print("삭제할 ID: ");
+        Long deleteId = sc.nextLong();
+
+        PreparedStatement deleteStatement = connection.prepareStatement("DELETE FROM member WHERE id  = ? ");
+        deleteStatement.setLong(1, deleteId);
+        int deleteResult = deleteStatement.executeUpdate();
+        System.out.println("삭제 후: " + deleteResult);
         // 데이터베이스 다 사용 후 종료 해줘야한다.
         connection.close();
     }
